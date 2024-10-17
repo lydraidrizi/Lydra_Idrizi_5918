0433a0c919ce00e526880213fa5fb95f5b48ad7b
student <student@uacs.edu.com>
2024-10-17 18:23:18 +0200

    ,

---
+++ exerice 2/src/Main.java
@@ -0,0 +1,35 @@
+public class Car {
+
+private String type;
+
+private int noSeats;
+
+private String fuelType;
+
+private String color;
+
+public String getType(){
+  return type;
+}
+
+public void setTypes(String type){
+
+    this.type = type;
+}
+
+public int getNoSeats() {
+
+ return noSeats;
+}
+
+public void setNoSeats(int noSeats){
+ this.noSeats = noSeats;
+}
+public String getFuelType(){
+
+    return fuelType;
+}
+
+public void setfuelType(String fuelType){
+
+this.fuelType = fuelType;
