8af27be4abb41656c0463e676b9d987087436932
student <student@uacs.edu.com>
2024-10-24 17:29:02 +0200

    .

---
+++ exercise02/exercise 1/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Egg{
+
+    public void Egg(){
+        System.out.println("Each egg has an albumen and a yolk.");
+    }
+    public void print(){
+        System.out.println("The egg is a nutritional product.");
+    }
+}
+    class OstrichEgg extends Egg{
+
+    @Override
+        public void Egg() {
+
+        System.out.println("The albumen and yolk of an osterich egg are the largest")
+    }
+
+
+    }
